2df9ecbf7135a8fd2060524effb78f4c4ddcbec7
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2020-12-18 15:24:01 -0500

    Update main.tf

--- 03-terraform-state/file-layout-example/stage/data-stores/mysql/main.tf
+++ 03-terraform-state/file-layout-example/stage/data-stores/mysql/main.tf
@@ -8,11 +8,11 @@ terraform {
     # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
     # manually, uncomment and fill in the config below.
 
-    # bucket         = "<YOUR S3 BUCKET>"
-    # key            = "<SOME PATH>/terraform.tfstate"
-    # region         = "us-east-2"
-    # dynamodb_table = "<YOUR DYNAMODB TABLE>"
-    # encrypt        = true
+    bucket         = "Terraformupandrunningnageshfisrstdatabse"
+    key            = "RDS/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "Dynamodbtableterraform-up-and-running-nagesh"
+    encrypt        = true
 
   }
 }
