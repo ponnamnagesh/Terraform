9c01bc61c66b1afb95846bf351bc97dec228ce87
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2020-12-23 16:54:28 -0500

    Update user-data.sh

--- 03-terraform-state/file-layout-example/stage/services/webserver-cluster/user-data.sh
+++ 03-terraform-state/file-layout-example/stage/services/webserver-cluster/user-data.sh
@@ -4,6 +4,7 @@ cat > index.html <<EOF
 <h1>Hello, World</h1>
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
+<h1>Are you stupid? No is the anwer so get this completed and move ahead in ur lyf</h1>
 EOF
 
 nohup busybox httpd -f -p ${server_port} &
