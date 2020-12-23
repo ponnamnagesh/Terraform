7f660ef13b9a8eb78d4a6fdbc832d4aed5536856
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2020-12-23 17:24:30 -0500

    Update variables.tf

--- 03-terraform-state/file-layout-example/stage/services/webserver-cluster/variables.tf
+++ 03-terraform-state/file-layout-example/stage/services/webserver-cluster/variables.tf
@@ -12,7 +12,8 @@ variable "db_remote_state_bucket" {
 variable "db_remote_state_key" {
   description = "The name of the key in the S3 bucket used for the database's remote state storage"
   type        = string
-  default     = "Remotesstatereadonlykey"
+  default     = "RDS"
+"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
